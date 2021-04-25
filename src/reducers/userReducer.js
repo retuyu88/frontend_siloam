145185c051a875ee3a22bbcfa579c16c48f876bd
andika <andika@moro.id>
2021-04-25 14:04:58 +0700

    update

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,25 @@
+import { CONSTANTS } from "../_actions";
+
+let user = JSON.parse(localStorage.getItem('user'))
+const initialState =
+    user ? { loggedIn: true, user} : {};
+
+
+export function authentication(state = initialState, action){
+    switch (action.type) {
+        case CONSTANTS.LOGIN_REQUEST:
+            return{
+                loggingIn:true,
+                user: action.user
+            };
+        case CONSTANTS.LOGIN_SUCCESS:
+            return{
+                loggedIn:true,
+                user:action.user
+            }
+        case CONSTANTS.LOGIN_FAILURE:
+            return {};
+        default:
+            return state;
+    }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
-import rootReducer from '../_reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
 
-const store = createStore(rootReducer);
+const initialState = {};
 
-export default store;
+const middleware = [thunk];
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+        applyMiddleware(...middleware),window.__REDUX_DEVTOOL_EXTENSION__ ? window.__REDUX_DEVTOOL_EXTENSION__() : (f) => (f)
+    )
+)
+
+export default store
